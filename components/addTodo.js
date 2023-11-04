35d1353142d798160a199b957c05e696f44244bc
Saba Phruidze <sabaphruidze01@gmail.com>
2023-11-04 21:07:09 +0400

    I created a new component where it become possible to add an app

---
+++ components/addTodo.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+
+const addTodo = () => {
+  return (
+    <View>
+      <Text>addTodo</Text>
+    </View>
+  );
+};
+
+export default addTodo;
