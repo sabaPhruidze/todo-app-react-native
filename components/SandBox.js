03ec5b5b3db843c5c8c900950ba8cd8aa62bba50
Saba Phruidze <sabaphruidze01@gmail.com>
2023-11-12 12:58:53 +0400

    flex Direction by default is column and we can change it to row

--- components/SandBox.js
+++ components/SandBox.js
@@ -2,7 +2,37 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function SandBox() {
-  return <Text>SandBox</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.boxOne}>One</Text>
+      <Text style={styles.boxTwo}>Two</Text>
+      <Text style={styles.boxThree}>Free</Text>
+      <Text style={styles.boxFour}>Four</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    paddingTop: 100,
+    backgroundColor: "#ddd",
+  },
+  boxOne: {
+    padding: 10,
+    backgroundColor: "violet",
+  },
+  boxTwo: {
+    padding: 10,
+    backgroundColor: "gold",
+  },
+  boxThree: {
+    padding: 10,
+    backgroundColor: "coral",
+  },
+  boxFour: {
+    padding: 10,
+    backgroundColor: "skyblue",
+  },
+});
